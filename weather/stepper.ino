59ee0d6f3e1f262af12820dfec19cbaade64e342
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2020-03-11 21:34:08 -0600

    adding raphael's code

---
+++ weather/stepper.ino
@@ -0,0 +1,44 @@
+
+#include <CheapStepper.h>
+
+CheapStepper stepper;
+// 8 <--> IN1
+// 9 <--> IN2
+// 10 <--> IN3
+// 11 <--> IN4
+
+int potPin = 0;    //potentiometer pin
+int val = 0;       //reading from potentiometer
+
+void setup() {
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+    
+    //Read Potentiometer
+    val = analogRead(potPin);
+
+    //Map the reading to 0~23 corresponding to 24 hours of the day
+    val = map(val, 0, 747, 0, 23);
+    if(val > 24){val = 23;}
+    //Map the reading to 0~4096 corresponding to the rotation of the motor
+    val = map(val, 0, 23, 0, 4095);
+    if(val > 4095){val = 4095;}
+
+    //Get current location of motor
+    int current = (int)stepper.getStep();
+
+    //Move the motor to match the value
+    if(current < val){
+      stepper.stepCW(); 
+      //counter--;
+    }
+    else if(current > val){
+      stepper.stepCCW();
+    }
+    else{
+      };
+
+  }
